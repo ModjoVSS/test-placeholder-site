01e1325c65f5982d8c1a394bd02c75b8e7f6428b
ModjoVSS <306863300+ModjoVSS@users.noreply.github.com>
2026-07-20 04:48:16 +0900

    Remember selected theme

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const themeButton = document.querySelector('#theme-toggle');
 
+if (localStorage.getItem('theme') === 'light') {
+  document.body.classList.add('light');
+}
+
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle('light');
+  const theme = document.body.classList.contains('light') ? 'light' : 'dark';
+  localStorage.setItem('theme', theme);
 });
